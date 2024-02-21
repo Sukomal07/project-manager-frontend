0bbfa63561439d5160325810bcd5b15d7b6d62b3
Sukomal07 <hypersukomal@gmail.com>
2024-02-21 11:13:00 +0530

    remove redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,15 @@ import ReactDOM from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App.jsx'
-import store, { persistor } from './redux/store.js'
+import store from './redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <App />
-        <Toaster />
-      </BrowserRouter>
-    </PersistGate>
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
   </Provider>
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,25 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { persistReducer, persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 
 import AnalyticsSlice from './slices/AnalyticsSlice.js'
 import AuthSlice from './slices/AuthSlice.js'
 
-const authPersistConfig = {
-    key: 'auth',
-    storage,
-};
-
-const persistedAuthReducer = persistReducer(authPersistConfig, AuthSlice);
-
 
 const store = configureStore({
     reducer: {
-        auth: persistedAuthReducer,
+        auth: AuthSlice,
         analytics: AnalyticsSlice
     },
     devTools: true
 })
 
-export const persistor = persistStore(store)
 export default store
